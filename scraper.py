40181856a7fd314b26dd5153280fb5fc38ace931
suresh <garimellasureshbabu@gmail.com>
2022-12-29 11:50:12 -0600

    capitalizing

--- scraper.py
+++ scraper.py
@@ -3,4 +3,4 @@ print("To develop an scraper import requests library")
 
 print("Inside master branch scraper code with new changes");
 
-print("made new changes in remote repository pull worked");
+print("Made new changes in remote repository pull worked");
